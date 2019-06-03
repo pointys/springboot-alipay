76c07524a40b831edbeb12f475cf05283029891f
pointys <1924552207@qq.com>
2019-06-03 12:44:52 +0800

    1.1version

---
+++ src/main/java/com/kicol/git/gitTest.java
@@ -0,0 +1,39 @@
+package com.kicol.git;
+
+import java.io.Serializable;
+
+/**
+ * @Author: 韩老魔
+ * @Date: 2019/6/3 0003 12:39
+ */
+public class gitTest implements Serializable {
+    private static final long serialVersionUID = -3606178821921526644L;
+
+    private Integer id;
+    private String name;
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
